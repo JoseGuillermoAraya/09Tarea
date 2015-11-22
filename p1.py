d8122b5abfed72969a219e83628ff51feda9b291
Jose Araya <joseguillermo.araya@gmail.com>
2015-11-22 18:14:09 -0300

    simulación bootstrap

--- p1.py
+++ p1.py
@@ -45,6 +45,35 @@ def func_a_minimizar_2(v, H):
     return f_modelo_2(params, v)
 
 
+def bootstrap(data, H_0):
+    ''' simulación de bootstrap para encontrar el
+    intervalo de  confianza (95%)'''
+    N, N1 = data.shape
+    N_boot = 10000
+    H = np.zeros(N_boot)
+    for i in range(N_boot):
+        s = np.random.randint(low=0, high=N, size=N)
+        fake_data = data[s][s]
+        distancia = fake_data[:, 0]
+        vel = fake_data[:, 1]
+        a_optimo_1, a_covarianza_1 = curve_fit(func_a_minimizar_1,
+                                           distancia, vel, 2)
+        a_optimo_2, a_covarianza_2 = curve_fit(func_a_minimizar_2,
+                                           vel, distancia, 2)
+        a_prom = (a_optimo_2 + a_optimo_1) / 2
+        H[i] = a_prom
+    fig2, ax2 = plt.subplots()
+    plt.hist(H, bins=30)
+    plt.axvline(H_0, color='r')
+    ax2.set_title("Simulacion de bootstrap")
+    ax2.set_xlabel("H [Km/s /Mpc]")
+    ax2.set_ylabel("frecuencia")
+    H = np.sort(H)
+    limite_bajo = H[int(N_boot * 0.025)]
+    limite_alto = H[int(N_boot * 0.975)]
+    print "El intervalo de confianza al 95% es: [{}:{}]".format(limite_bajo, limite_alto)
+
+
 # Main
 data = np.loadtxt("data/hubble_original.dat")
 distancia = data[:, 0]
@@ -55,4 +84,7 @@ a_optimo_1, a_covarianza_1 = curve_fit(func_a_minimizar_1,
 a_optimo_2, a_covarianza_2 = curve_fit(func_a_minimizar_2,
                                    vel, distancia, 2)
 a_prom = (a_optimo_2 + a_optimo_1) / 2
+H_0 = a_prom
 mostrar_datos(distancia, vel, a_optimo_1, a_optimo_2, a_prom)
+
+intervalo_confianza = bootstrap(data, H_0)
