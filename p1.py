c80ca4fbdfa048aee4915177f34e9ec6abd10a06
Jose Araya <joseguillermo.araya@gmail.com>
2015-11-22 16:25:41 -0300

    graficar datos archivo

---
+++ p1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+
+''' Script que deriva la constante de Hubble incluyendo su intervalo de
+co nfianza al 95 "%" a partir de los datos en data/hubble_original.dat '''
+
+
+def mostrar_datos(distancia, vel):
+    ax, fig = plt.subplots()
+    plt.scatter(distancia, vel)
+    fig.set_title("Datos originales y ajuste lineal")
+    fig.set_xlabel("Distancia [Mpc]")
+    fig.set_ylabel("Velocidad [Km/s]")
+    plt.show()
+
+# Main
+data = np.loadtxt("data/hubble_original.dat")
+distancia = data[:, 0]
+vel = data[:, 1]
+mostrar_datos(distancia, vel)
