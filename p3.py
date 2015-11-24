8f82b9442444b156c87abc5be79a511756cfdbbe
Jose Araya <joseguillermo.araya@gmail.com>
2015-11-24 01:18:35 -0300

    arreglo plots p3

--- p3.py
+++ p3.py
@@ -12,14 +12,15 @@ co nfianza al 95 "%" a partir de los datos en data/DR9Q.dat '''
 def mostrar_datos(banda_i, error_i, banda_z, error_z, c):
     '''grafica los datos originales con sus errores asociados,
     grafica el ajuste lineal polyfit'''
-    ax, fig = plt.subplots()
-    fig.errorbar(banda_i, banda_z, xerr=error_i, yerr=error_z, fmt="o",
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+    ax1.errorbar(banda_i, banda_z, xerr=error_i, yerr=error_z, fmt="o",
                  label="Datos originales y ajuste lineal")
     x = np.linspace(-100, 500, 600)
-    fig.plot(x, c[1] + x*c[0], color="r", label="ajuste lineal")
-    fig.set_title("Datos originales")
-    fig.set_xlabel("Flujo banda i [$10^{-6}Jy$]")
-    fig.set_ylabel("Flujo banda z [$10^{-6}Jy$]")
+    ax1.plot(x, c[1] + x*c[0], color="r", label="ajuste lineal")
+    ax1.set_title("Datos originales")
+    ax1.set_xlabel("Flujo banda i [$10^{-6}Jy$]")
+    ax1.set_ylabel("Flujo banda z [$10^{-6}Jy$]")
     plt.legend(loc=2)
     plt.savefig("bandas.jpg")
 
@@ -35,21 +36,22 @@ def mc(banda_i, error_i, banda_z, error_z, c_0):
         muestra_i = banda_i + error_i * r
         muestra_z = banda_z + error_z * r
         pendiente[j], cte[j] = np.polyfit(muestra_i, muestra_z, 1)
-    ax2, fig2 = plt.subplots()
-    fig2.hist(pendiente, bins=30)
-    fig2.axvline(c[0], color='r')
-    fig2.set_title("Simulacion de Montecarlo (pendientes)")
-    fig2.set_xlabel("pendiente [adimensional]")
-    fig2.set_ylabel("frecuencia")
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+    ax2.hist(pendiente, bins=30)
+    ax2.axvline(c[0], color='r')
+    ax2.set_title("Simulacion de Montecarlo (pendientes)")
+    ax2.set_xlabel("pendiente [adimensional]")
+    ax2.set_ylabel("frecuencia")
     plt.savefig("mc.jpg")
 
-    ax3, fig3 = plt.subplots()
-    fig3.hist(pendiente, bins=30)
-    fig3.axvline(c[0], color='r')
-    fig3.set_title("Simulacion de Montecarlo (coef de posicion)")
-    fig3.set_xlabel("coef de posicion [adimensional]")
-    fig3.set_ylabel("frecuencia")
-    plt.show()
+    fig3 = plt.figure()
+    ax3 = fig3.add_subplot(111)
+    ax3.hist(pendiente, bins=30)
+    ax3.axvline(c[0], color='r')
+    ax3.set_title("Simulacion de Montecarlo (coef de posicion)")
+    ax3.set_xlabel("coef de posicion [adimensional]")
+    ax3.set_ylabel("frecuencia")
     plt.savefig("mc_2.jpg")
 
     pendiente = np.sort(pendiente)
@@ -73,5 +75,6 @@ banda_z = data[:, 2] * 3.631
 error_z = data[:, 3] * 3.631
 c = np.polyfit(banda_i, banda_z, 1)
 print(c)
-mostrar_datos(banda_i, error_i, banda_z, error_z, c)
 intervalo_confianza = mc(banda_i, error_i, banda_z, error_z, c)
+mostrar_datos(banda_i, error_i, banda_z, error_z, c)
+plt.show()
